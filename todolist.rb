4ad631e8fd209609b348bc0401d3d6b7aefc51dd
Diane <diane@code12labs.com>
2016-04-17 00:00:40 -0500

    Reworked todolist and add new_features

--- todolist.rb
+++ todolist.rb
@@ -1,9 +1,10 @@
 class TodoList
-  attr_accessor :title, :items
+  attr_accessor :title, :items, :user
 
   # Initialize to-do list with a title and no items
-  def initialize(list_title)
+  def initialize(list_title, user)
     @title = list_title
+    @user = user
     @items = Array.new
   end
 
@@ -13,42 +14,72 @@ class TodoList
   end
 
   # Method to add items to list
-  def add_item(new_item)
-    item = Item.new(new_item)
+  def add_item(new_item, completion_status = false, due_date)
+    item = Item.new(new_item, completion_status, due_date)
     @items << item
   end
 
+  def toggle_status(index)
+    @items[index].toggle_status
+  end
+
+  # Method to add due date
+  def due_date(date)
+    @due_date = "#{date.strftime("%d-%m-%Y")}"
+  end
+
   # Method to delete items from list
-  def remove_item(item)
-    @items.delete(item)
+  def remove_item(index)
+    @items.delete(index)
   end
 
   # Method to update the completion status of an item on the list
-  def update_status
+  def completed?(index)
+    return @items.all? {|item| item.completion_status == true}
   end
 
   # Method to print todo list
   def print_list
+    puts @name
+
+    @items.each do |item|
+      item.print_item
+    end
   end
 
 end
 
 class Item
-  attr_accessor :description, :completed_status
+  attr_accessor :description, :completed_status, :priority
 
   # Initialize item with a description and marked as not complete
-  def initialize(item_description)
+  def initialize(item_description, completion_status, due_date)
     @description = item_description
-    @completed_status = false
+    @completion_status = false
+  end
+
+  def toggle_status
+    @completion_status = !@completion_status
   end
 
   # Method to see if an item on the list is completed (boolean)
   def completed?
-    @completed_status = false || true
+    @completion_status = false || true
+  end
+
+  # Method to say if item is high priority or not
+  def high_priority
+    @priority = false || true
+  end
+
+  # Method to add due date
+  def due_date(date)
+    @due_date = "#{date.strftime("%Y-%m-%d")}"
   end
 
   # Method that formats the description and completion status of items
-  def printing_items
+  def print_item
+    puts "[#{@completed ? "X" : " "}] #{@description}"
   end
 
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,37 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+list = TodoList.new("Diane's List", "Diane")
 
 # Add four new items
+list.add_item("item_1", "true", "04Feb2016")
+list.add_item("item_2", "false", "04Aug2016")
+list.add_item("item_3", "false", "04Jun2016")
+list.add_item("item_4", "true", "04Jul2016")
 
 # Print the list
+list.print_list
 
 # Delete the first item
+list.remove_item(1)
 
 # Print the list
+list.print_list
 
 # Delete the second item
+list.remove_item(2)
 
 # Print the list
+list.print_list
 
 # Update the completion status of the first item to complete
+list.completed?(1)
 
 # Print the list
+list.print_list
 
 # Update the title of the list
+list.rename("My New List")
 
 # Print the list
+list.print_list
